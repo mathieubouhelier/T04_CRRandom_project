f03034325a0072bf7b3da7ee22b0b02b3854e3b6
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-06-11 11:55:49 -0300

    segunda solucao feature random

--- app/js/controllers/Controller.js
+++ app/js/controllers/Controller.js
@@ -57,47 +57,47 @@ class Controller{
     //   nomes[currentIndex] = nomes[randomIndex];
     //   nomes[randomIndex] = temporaryValue;
   // }
-   randomize(array){
-      const original = [...array];
-      let result = [];
-      let index = 0;
-      while (array.length > 0) {
-        const i = Math.floor(Math.random() * array.length);
-        result[index] = array[i];
-        if(result[index] !== original[index]) {
-          index++;
-          array.splice(i, 1);
+  //  randomize(array){
+  //     const original = [...array];
+  //     let result = [];
+  //     let index = 0;
+  //     while (array.length > 0) {
+  //       const i = Math.floor(Math.random() * array.length);
+  //       result[index] = array[i];
+  //       if(result[index] !== original[index]) {
+  //         index++;
+  //         array.splice(i, 1);
+  //       }
+  //     }
+  //     this.updateRandomizeView(result);
+  //   }
+    
+    randomize(array) {
+      const originalPositions = array.map((user) => user.nome);
+      const nomes = originalPositions;
+      var currentIndex = nomes.length,
+        temporaryValue,
+        randomIndex;
+      // While existem itens
+      while (0 !== currentIndex) {
+        // Seleciona um item restante e aleatório
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        // Verifica se os itens trocados estão em posições diferentes do array original (originalPositions[])
+        if (
+          originalPositions[currentIndex - 1] !== nomes[randomIndex] &&
+          originalPositions[randomIndex] !== nomes[currentIndex - 1]
+        ) {
+          // Seleciona o proximo item do array
+          currentIndex -= 1;
+          // Troca a posição dos elementos.
+          temporaryValue = nomes[currentIndex];
+          nomes[currentIndex] = nomes[randomIndex];
+          nomes[randomIndex] = temporaryValue;
+          // Se currentPosition - 1 for igual a 0, sai do loop
+        } else if (currentIndex - 1 === 0) {
+          currentIndex = 0;
         }
       }
-      this.updateRandomizeView(result);
+      return nomes;
     }
-    
-    // function randomize(array) {
-    //   const originalPositions = array.map((user) => user.nome);
-    //   const nomes = originalPositions;
-    //   var currentIndex = nomes.length,
-    //     temporaryValue,
-    //     randomIndex;
-    //   // While existem itens
-    //   while (0 !== currentIndex) {
-    //     // Seleciona um item restante e aleatório
-    //     randomIndex = Math.floor(Math.random() * currentIndex);
-    //     // Verifica se os itens trocados estão em posições diferentes do array original (originalPositions[])
-    //     if (
-    //       originalPositions[currentIndex - 1] !== nomes[randomIndex] &&
-    //       originalPositions[randomIndex] !== nomes[currentIndex - 1]
-    //     ) {
-    //       // Seleciona o proximo item do array
-    //       currentIndex -= 1;
-    //       // Troca a posição dos elementos.
-    //       temporaryValue = nomes[currentIndex];
-    //       nomes[currentIndex] = nomes[randomIndex];
-    //       nomes[randomIndex] = temporaryValue;
-    //       // Se currentPosition - 1 for igual a 0, sai do loop
-    //     } else if (currentIndex - 1 === 0) {
-    //       currentIndex = 0;
-    //     }
-    //   }
-    //   return nomes;
-    // }
 }
